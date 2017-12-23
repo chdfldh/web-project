cace1796a89779b65290baa0cfa72e164c5efc66
FULLNAM-54CHV64\Administrator <chdfldh@naver.com>
2017-12-23 17:41:34 +0900

    채용사이트 회사 리스트 구현

--- frontend/src/routes/Company.js
+++ frontend/src/routes/Company.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import './Company.css';
+
+import Card from '../components/Card';
+import axios from 'axios';
 
 class Company extends React.Component { 
     
-    render(){
-        return(
-            <div>Company</div>
+        
+        constructor(){
+            super();
+            
+            this.state = {companyArray : []
+        }
+    }
+        
+        componentDidMount(){
+            axios.get('http://localhost:4000/company')
+                .then((response)=>{
+                
+                    const data = this.state.companyArray.concat(response.data.company);
+                    this.setState({ companyArray : data });
+            });
+        }
+    
+         render(){
+        
+        const { companyArray } = this.state;
+             
+        const list = companyArray.map((v)=>{
+           
+            return (
+                <Card 
+                    key={v.id}
+                    company={v.name}
+                    recruit={v.recruit}
+                    rebate={v.rebate}
+                    recom={v.recommendation}
+                    favorite={v.favorite}
+                />
+                )
+        });
+             
+        return (
+             
+            <div>
+            <div className="list">{list}</div>
+        </div> 
+             
         )
     }
 }
 
 export default Company;
+
+
+
